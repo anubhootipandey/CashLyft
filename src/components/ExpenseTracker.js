addc366bac820963d9b1e18dff63ec6affb6cefb
anubhootipandey <anubhootipandey05@gmail.com>
2024-10-19 22:05:58 +0530

    added responsiveness

--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../App.css';
+import '../App.css'; // Make sure to import the CSS
 
 function ExpenseTracker() {
   const [expenses, setExpenses] = useState(() => {
